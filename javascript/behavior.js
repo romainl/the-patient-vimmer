1cfe559e93dc9161bed6bb8a488c09fb74321a02
Romain Lafourcade <romainlafourcade@new-host.home>
2015-02-25 00:26:25 +0100

    Play/Stop gifcasts.

--- javascript/behavior.js
+++ javascript/behavior.js
@@ -1,1 +1,17 @@
-console.log("foo");
+function toggleGif(event) {
+    var gif_url = $(this).attr("href");
+    var png_url = "images/gifcast_off.png";
+    var image = $(this).find("img");
+
+    if (image.attr("src") === gif_url) {
+        image.attr("src", png_url).load();
+    } else if (image.attr("src") === png_url) {
+        image.attr("src", gif_url).load();
+    }
+
+    event.preventDefault();
+}
+
+$(document).ready(function() {
+    $("body").on("click", "a.image", toggleGif);
+});
